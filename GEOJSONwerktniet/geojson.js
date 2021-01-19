80d9b31fb0b5ff336f1406e189b89ffa0c5520fb
sweksparrow <71262814+sweksparrow@users.noreply.github.com>
2021-01-19 16:29:59 +0100

    helppls

---
+++ GEOJSONwerktniet/geojson.js
@@ -0,0 +1,89 @@
+var geojson = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              106.03531837463379,
+              10.332651927640583
+            ]
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              105.18310546875,
+              9.143367559778994
+            ]
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              105.7770538330078,
+              10.024638805124969
+            ]
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              106.69095754623413,
+              10.772792817972395
+            ]
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              105.7986831665039,
+              9.58983267584058
+            ]
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "LineString",
+            "coordinates": [
+              [
+                105.18311083316803,
+                9.143372856035686
+              ],
+              [
+                105.7986831665039,
+                9.589874991466626
+              ],
+              [
+                105.77706456184387,
+                10.024654652682777
+              ],
+              [
+                106.03540420532227,
+                10.332905243850371
+              ],
+              [
+                106.69095754623413,
+                10.77279808784851
+              ]
+            ]
+          }
+        }
+      ]
+    }}
